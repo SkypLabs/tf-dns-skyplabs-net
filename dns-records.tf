fcb40888cb481e32a7fac50491068c9a9688a9aa
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2020-02-07 18:57:12 +0000

    Reorganise DNS records

--- dns-records.tf
+++ dns-records.tf
@@ -2,6 +2,9 @@ locals {
   zone = "skyplabs.net"
 
   dns_records = [
+    # -------------------------------------------------- #
+    # DNS zone configuration
+    # -------------------------------------------------- #
     {
       name = ""
       type = "NS"
@@ -13,58 +16,10 @@ locals {
     },
     {
       name = ""
-      type = "MX"
-      ttl  = 0,
-      targets = [
-        "1 mail.tutanota.de.",
-      ]
-    },
-    {
-      name = ""
       type = "A"
       ttl  = 0,
       targets = [
         "213.186.33.5",
-      ]
-    },
-    {
-      name = ""
-      type = "TXT"
-      ttl  = 60,
-      targets = [
-        "\"4|https://blog.skyplabs.net\"",
-      ]
-    },
-    {
-      name = ""
-      type = "TXT"
-      ttl  = 0,
-      targets = [
-        "\"brave-ledger-verification=a3b2ef122a15353fdaaf5bfbbe25bc0976d14bf8cc86ff91b8eba878c1da56f1\"",
-      ]
-    },
-    {
-      name = ""
-      type = "TXT"
-      ttl  = 0,
-      targets = [
-        "\"keybase-site-verification=r4YLk-AeFOVRhrahHCOUxX-Ocv0N6JMMh9L8wHLo_co\"",
-      ]
-    },
-    {
-      name = ""
-      type = "TXT"
-      ttl  = 0,
-      targets = [
-        "\"<script>alert('OPENBUGBOUNTY')</script>\"",
-      ]
-    },
-    {
-      name = ""
-      type = "SPF"
-      ttl  = 600,
-      targets = [
-        "\"v=spf1 include:spf.tutanota.de -all\"",
       ]
     },
     {
@@ -81,6 +36,63 @@ locals {
       ttl  = 0,
       targets = [
         "hosting.gitbook.com.",
+      ]
+    },
+    {
+      name = ""
+      type = "TXT"
+      ttl  = 60,
+      targets = [
+        "\"4|https://blog.skyplabs.net\"",
+      ]
+    },
+    # -------------------------------------------------- #
+    # Email configuration
+    # -------------------------------------------------- #
+    {
+      name = ""
+      type = "MX"
+      ttl  = 0,
+      targets = [
+        "1 mail.tutanota.de.",
+      ]
+    },
+    {
+      name = ""
+      type = "SPF"
+      ttl  = 600,
+      targets = [
+        "\"v=spf1 include:spf.tutanota.de -all\"",
+      ]
+    },
+    # -------------------------------------------------- #
+    # Domain verification
+    # -------------------------------------------------- #
+    {
+      name = ""
+      type = "TXT"
+      ttl  = 0,
+      targets = [
+        "\"keybase-site-verification=r4YLk-AeFOVRhrahHCOUxX-Ocv0N6JMMh9L8wHLo_co\"",
+      ]
+    },
+    {
+      name = ""
+      type = "TXT"
+      ttl  = 0,
+      targets = [
+        "\"brave-ledger-verification=a3b2ef122a15353fdaaf5bfbbe25bc0976d14bf8cc86ff91b8eba878c1da56f1\"",
+      ]
+    },
+    # -------------------------------------------------- #
+    # Misc.
+    # -------------------------------------------------- #
+    {
+      name = ""
+      type = "TXT"
+      ttl  = 0,
+      targets = [
+        "\"<script>alert('OPENBUGBOUNTY')</script>\"",
       ]
     },
     {
